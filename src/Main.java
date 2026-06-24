6050cb217405c5c985dfc73c98faf7d4ce236061
LoqmanSamani <samaniloqman91@gmail.com>
2026-06-24 14:26:47 +0200

    first comment!

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String [] args){
+        // for writing comment in java we use two slashes
+        /* for multiline comment we
+        use this format
+        * */
+        System.out.print("I like pizza!\n");
+        System.out.println("it's really good!"); // ln at the end of print does the same as /n in the line above.
+        System.out.println("buy me a pizza!");
+        System.out.print("this was the first section of learning java:)");
+    }
+}
