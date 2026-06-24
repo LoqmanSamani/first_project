ef6a0973a4d46ecf61090d4d8b8411d9d20f6113
LoqmanSamani <samaniloqman91@gmail.com>
2026-06-24 16:27:27 +0200

    src/Variables.java has been modified!

--- src/Variables.java
+++ src/Variables.java
@@ -8,8 +8,33 @@ public class Variables {
         // 2. assignment
 
         int age;
-        age = 25;
-        System.out.println(age);
+        int year;
+        double distance;
+        char my_init;
+	boolean is_married;
+	String my_name;
 
+
+        age  = 25;
+	year = 2026;
+	distance = 26.7;
+	my_init  = 'L';
+	my_name = "Loqman";
+	is_married  = false;
+	
+	if(is_married){
+    		System.out.println("congradulations! you already fucked up :)");
+       }else{
+		System.out.println("you are the most free man i have ever seen in my live!!!");       
+       }
+
+        System.out.println(age);
+	System.out.println(year);
+	System.out.println(distance);
+	System.out.println("the year is " + year);
+	System.out.println("and i am " + age + " years old :)");
+	System.out.println("my name is " + my_name + " and i am not married!");
+	System.out.println(is_married);
+	System.out.println(my_init);
     }
 }
