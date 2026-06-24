3b667c90cc31bbfb186ae6c25d04f8c01d0a36f2
LoqmanSamani <samaniloqman91@gmail.com>
2026-06-24 15:35:56 +0200

    variables.java has been added!

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main (String[] args) {
+        // in this section we present
+        // the concept of variable in java!
+
+        // steps to create a variable:
+        // 1. declaration
+        // 2. assignment
+
+        int age;
+        age = 25;
+        System.out.println(age);
+
+    }
+}
