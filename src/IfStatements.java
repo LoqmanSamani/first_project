01168a4f239003a407159c010ed802c465b0cc9d
LoqmanSamani <samaniloqman91@gmail.com>
2026-06-29 19:03:59 +0200

    #11: IfStatements.java has been modified!

--- src/IfStatements.java
+++ src/IfStatements.java
@@ -2,14 +2,17 @@ public class IfStatements{
 
 	public static void main(String[] args){
 		// if statements in java
-		int[] ages = {-10, 10, 54, 28, 0, 18};
+		int[] ages = {-10, 10, 54, 28, 0, 18, 85};
    		for (int age: ages){
 
-			if(age >= 18){
+			if(age >= 18 && age <= 60){
 				System.out.println("You are an adult!");
 			}
 			else if(age <= 0){
 				System.out.println("It's not possible to have negative age!");
+			}
+			else if (age > 60){
+				System.out.println("You are a senior!");
 			}
 			else{
 				System.out.println("You are a child!");
